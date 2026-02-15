da84bc86188d294c0ee6bd8ac9634d209f994886
Harsh Uppal <harshuppal300@gmail.com>
2026-02-15 13:07:04 +0530

    slight changes in the diagonal question

--- DSA/Q35(print both diagonal of a matrix).py
+++ DSA/Q35(print both diagonal of a matrix).py
@@ -1,12 +1,12 @@
-a = [[1,2,3],
-     [4,5,6],
-     [7,8,9]]
-def diagonal(p):
-    for i in range(len(a)):
-        print(a[i][i])
-        a[i].reverse()
-    print("\n")
-    for i in range(len(a)):
-        print(a[i][i])
-    return p
-diagonal(a)
+a = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+b = []
+c = []
+def diagonal(p, q, r):
+    n = len(p)
+    for i in range(n):
+        q.append(p[i][i])
+        r.append(p[i][n-i-1])
+    return q, r
+print(diagonal(a, b, c))
